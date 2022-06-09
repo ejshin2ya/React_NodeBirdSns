e88dc1c81b169e3a569e677e1a00fdd4afa0c540
신은정 <ejshin0213@naver.com>
2022-06-09 23:31:25 +0900

    게시글 삭제 saga 작성하기

--- components/PostCard.js
+++ components/PostCard.js
@@ -8,12 +8,15 @@ import {
 import { Avatar, Button, Card, Comment, List, Popover } from "antd";
 import PropTypes from "prop-types";
 import React, { useState, useCallback } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { REMOVE_POST_REQUEST } from "../reducers/post";
 import CommentForm from "./CommentForm";
 import PostCardContent from "./PostCardContent";
 import PostImages from "./Postimages";
 
 const PostCard = ({ post }) => {
+  const dispatch = useDispatch();
+  const { removePostLoading } = useSelector((state) => state.post);
   const [liked, setLiked] = useState(false);
   const [commentFormOpened, setCommentFormOpened] = useState(false);
   const onToggleLike = useCallback(() => {
@@ -22,6 +25,14 @@ const PostCard = ({ post }) => {
   const onToggleComment = useCallback(() => {
     setCommentFormOpened((prev) => !prev);
   });
+
+  const onRemovePost = useCallback(() => {
+    dispatch({
+      type: REMOVE_POST_REQUEST,
+      data: post.id,
+    });
+  });
+
   const id = useSelector((state) => state.user.me?.id);
   return (
     <div style={{ marginBottom: 20 }}>
@@ -46,7 +57,13 @@ const PostCard = ({ post }) => {
                 {id && post.User.id === id ? (
                   <>
                     <Button>수정</Button>
-                    <Button type="danger">삭제</Button>
+                    <Button
+                      type="danger"
+                      loading={removePostLoading}
+                      onClick={onRemovePost}
+                    >
+                      삭제
+                    </Button>
                   </>
                 ) : (
                   <Button>신고</Button>
