b4710cf4e9f51a6f9463a1d269889ff12ef8bc89
신은정 <ejshin0213@naver.com>
2022-06-09 18:15:07 +0900

    바뀐 상태 적용하고 eslint 점검하기

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -5,7 +5,7 @@ import { logoutRequestAction } from "../reducers/user";
 
 const UserProfile = () => {
   const dispatch = useDispatch();
-  const { me, isLoggingOut } = useSelector((state) => state.user);
+  const { me, logOutLoading } = useSelector((state) => state.user);
   const onLogOut = useCallback(() => {
     dispatch(logoutRequestAction());
   }, []);
@@ -13,13 +13,16 @@ const UserProfile = () => {
     <Card
       actions={[
         <div key="twit">
-          짹짹 <br />0
+          짹짹 <br />
+          {me.Posts.length}
         </div>,
         <div key="followings">
-          팔로잉 <br />0
+          팔로잉 <br />
+          {me.Followings.length}
         </div>,
         <div key="followings">
-          팔로워 <br />0
+          팔로워 <br />
+          {me.Followers.length}
         </div>,
       ]}
     >
@@ -28,7 +31,7 @@ const UserProfile = () => {
         title={me.nickname}
       />
 
-      <Button onClick={onLogOut} loading={isLoggingOut}>
+      <Button onClick={onLogOut} loading={logOutLoading}>
         로그아웃
       </Button>
     </Card>
--- pages/signup.js
+++ pages/signup.js
@@ -4,12 +4,15 @@ import Head from "next/head";
 import { Button, Checkbox, Form, Input } from "antd";
 import useInput from "../hooks/useInput";
 import styled from "styled-components";
-
+import { SIGN_UP_REQUEST } from "../reducers/user";
+import { useSelector, useDispatch } from "react-redux";
 const ErrorMessage = styled.div`
   color: red;
 `;
 const Signup = () => {
-  const [id, onChangeId] = useInput("");
+  const dispatch = useDispatch();
+  const { signUpLoading } = useSelector((state) => state.user);
+  const [email, onChangeEmail] = useInput("");
   const [nickname, onChangeNickname] = useInput("");
   const [password, onChangePassword] = useInput("");
   const [passwordCheck, setPasswordCheck] = useState("");
@@ -36,8 +39,12 @@ const Signup = () => {
     if (!term) {
       return setTermError(true);
     }
-    console.log(id, nickname, password);
-  }, [password, passwordCheck, term]);
+    console.log(email, nickname, password);
+    dispatch({
+      type: SIGN_UP_REQUEST,
+      data: { email, password, passwordCheck, term },
+    });
+  }, [email, password, passwordCheck, term]);
 
   return (
     <AppLayout>
@@ -47,9 +54,15 @@ const Signup = () => {
       </Head>
       <Form onFinish={onSubmit}>
         <div>
-          <label htmlFor="user-id">아이디</label>
+          <label htmlFor="user-email">이메일</label>
           <br />
-          <Input name="user-id" value={id} required onChange={onChangeId} />
+          <Input
+            name="user-email"
+            type="email"
+            value={email}
+            required
+            onChange={onChangeEmail}
+          />
         </div>
         <div>
           <label htmlFor="user-nick">닉네임</label>
@@ -93,7 +106,7 @@ const Signup = () => {
           {termError && <ErrorMessage>약관에 동의하셔야 합니다.</ErrorMessage>}
         </div>
         <div style={{ marginTop: 10 }}>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" htmlType="submit" loading={signUpLoading}>
             가입하기
           </Button>
         </div>
